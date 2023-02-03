2153a8272d25f075860d16388c7aca191211506f
Cordione <52832668+Cordione@users.noreply.github.com>
2023-02-03 05:59:31 +0100

    Added Test for food parser

---
+++ src/test/ts/foodParser.test.ts
@@ -0,0 +1,13 @@
+import { foodParser } from '../../functions/parsers/foodParser';
+
+describe('Food tests', () => {
+    test('Should properly display food and ingredients', () => {
+        //given
+        const filePath = './src/test/csv/food.csv';
+        //when
+        const result = foodParser(filePath);
+        //then
+        expect(result[0].name).toEqual('Emperor Chicken');
+        expect(result[0].ingerdients).toContain(['Fat Cat Chicken', 'Spicy Sauce', 'Tuna Cake']);
+    });
+});
