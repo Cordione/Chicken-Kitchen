9500ea510dac584e877df859b8baa45a7c2e3d98
Cordione <52832668+Cordione@users.noreply.github.com>
2023-02-08 17:59:26 +0100

    updated CSV FILES to match 1 changes, updated parsers to match 1 changes, moved utils to parsers as they're used there, updated interfaces to match 1 changes

--- src/functions/parsers/baseIngredientsParser.ts
+++ src/functions/parsers/baseIngredientsParser.ts
@@ -1,5 +1,5 @@
 import { IBaseIngredients } from '../../Interface/IBaseIngredients';
-import { createRawArray } from '../../utils/createRawArray';
+import { createRawArray } from './utils/createRawArray';
 
 export function baseIngredientsParser(sourceString: string) {
     const rawArray = createRawArray(sourceString).flat();
@@ -8,12 +8,14 @@ export function baseIngredientsParser(sourceString: string) {
     const baseIngredients: IBaseIngredients[] = [];
     //Run throught recived output array
     //Trim all inputs nicely
+
     for (let index = 0; index < rawArray.length; index++) {
-        const singleWord = rawArray[index].trim();
-        if (singleWord.length > 0 && singleWord != '') {
-            baseIngredients.push({ name: singleWord });
+        const singleWord = rawArray[index].split(':');
+        const costAsNumber = parseInt(singleWord[1]);
+        if (singleWord.length > 0 && singleWord[0] != '') {
+            baseIngredients.push({ name: singleWord[0].trim(), cost: costAsNumber });
         }
     }
     return baseIngredients;
 }
-// console.log(baseIngredientsParser('./src/csv_files/baseIngredients.csv'));
+console.log(baseIngredientsParser('./src/csv_files/baseIngredients.csv'));
--- src/functions/parsers/customersParser.ts
+++ src/functions/parsers/customersParser.ts
@@ -1,6 +1,6 @@
-import { trimMe } from '../../utils/trimMe';
+import { trimMe } from './utils/trimMe';
 import { ICustomerAlergies } from '../../Interface/ICustomerAlergies';
-import { createRawArray } from '../../utils/createRawArray';
+import { createRawArray } from './utils/createRawArray';
 export function customersParser(sourceString: string) {
     //Pass recived input to fs.fileReadSync as source
     //Split recived input by new line \n\r
@@ -13,13 +13,14 @@ export function customersParser(sourceString: string) {
     //Trim all inputs nicely
     for (let index = 0; index < rawArray.length; index++) {
         const singleLine = rawArray[index].filter(x => x != '');
-        const secondToNColumn = trimMe(singleLine);
-        if (singleLine.length == 1) {
-            customerArray.push({ customerName: singleLine[0], alergies: [] });
-        } else if (singleLine.length > 1) {
-            customerArray.push({ customerName: singleLine[0], alergies: secondToNColumn });
+        const secondToNColumn = trimMe(singleLine, 1);
+        const customerBudget = parseInt(singleLine[singleLine.length - 1]);
+        if (singleLine.length == 2) {
+            customerArray.push({ customerName: singleLine[0], alergies: [], budget: customerBudget });
+        } else if (singleLine.length > 2) {
+            customerArray.push({ customerName: singleLine[0], alergies: secondToNColumn, budget: customerBudget });
         }
     }
     return customerArray;
 }
-// console.log(customersParser('./src/csv_files/customersAlergies.csv'));
+console.log(customersParser('./src/csv_files/customersAlergies.csv'));
