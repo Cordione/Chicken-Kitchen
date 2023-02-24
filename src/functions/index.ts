2cb063b3fe35d23cedc8f8050d90a31b15b72726
Cordione <52832668+Cordione@users.noreply.github.com>
2023-02-24 20:09:47 +0100

    refactored main function plus take order

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,87 +1,34 @@
+import { ICommandAndParameters } from '../Interface/ICommandAndParameters';
 import { IRestaurant } from '../Interface/IRestaurant';
 import { createReport } from './createReport';
 import { baseIngredientsParser } from './parsers/baseIngredientsParser';
 import { customersParser } from './parsers/customersParser';
 import { foodParser } from './parsers/foodParser';
 import { inputParser } from './parsers/inputParser';
+import { inputParserWithoutFile } from './parsers/inputParserWithoutFile';
 import { takeOrder } from './takeOrder';
 
-export function main(...args: string[]) {
+export function main(initialString?: string) {
     const customers = customersParser('./src/csv_files/customersAlergies.csv');
     const food = foodParser('./src/csv_files/food.csv');
     const baseIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
-    const command: string[] = [];
-    const customerIngredientSign: string[] = [];
-    const orderQuantity: string[] = [];
-    //If we would wish to have args input
     const finalOutput: string[] = [];
     const restaurant: IRestaurant = {
         budget: 500,
     };
     const restaurantBudgetIterations: number[] = [];
-    restaurantBudgetIterations.push(restaurant.budget);
-    if (args.length > 0) {
-        if (args.length % 3 != 0) {
-            return 'You have to pass a proper input';
-        } else {
-            // -Third parameter must be a numeric value greater than 0
-            // -If is not a numeric value or lower than 0 throw error
-            // - Third parameter must be numeric value can be positive or negative.
-
-            for (let index = 0; index < args.length; index++) {
-                if (index % 3 == 0) {
-                    if (args[index].toLowerCase() != 'Buy'.toLowerCase() && args[index].toLowerCase() != 'Order'.toLowerCase() && args[index].toLowerCase() != 'Budget'.toLowerCase()) {
-                        throw new Error('Unknown command');
-                    } else {
-                        command.push(args[index]);
-                    }
-                }
-
-                if (index % 3 == 1) {
-                    if (command[Math.ceil(index / 3) - 1].toLowerCase() == 'buy'.toLowerCase()) {
-                        customerIngredientSign.push(args[index]);
-                    } else if (command[Math.ceil(index / 3) - 1].toLowerCase() == 'Order'.toLowerCase()) {
-                        // If command equals to "Order":
-                        // -Ensure that second parameter case insensitive exist in base ingredients array
-                        // -If no throw error.
-                        if (baseIngredients.find(ingredient => ingredient.name.toLowerCase() == args[index].toLowerCase())) {
-                            //customerList is supposed to be renamed, have no clue how to call it right now, but leaving it as check mark.
-                            customerIngredientSign.push(args[index]);
-                        } else {
-                            throw new Error('You want to order unknown ingredient');
-                        }
-                    } else if (command[Math.ceil(index / 3) - 1].toLowerCase() == 'budget'.toLowerCase()) {
-                        //Right now to be skipped, focusing on ORDER.
-                        // If commands equals to "Budget":
-                        // - Second parameter must equal to one of those "=", "-", "+"
-                        if (args[index] == '=' || args[index] == '-' || args[index] == '+') {
-                            customerIngredientSign.push(args[index]);
-                        } else {
-                            throw new Error(`You passed wrong sign, acceptable signs are: - + = `);
-                        }
-                    }
-                } else if (index % 3 == 2) {
-                    const isProperNumber = parseInt(args[index]);
-                    if (command[Math.ceil(index / 3) - 1].toLowerCase() == 'buy'.toLowerCase()) {
-                        orderQuantity.push(args[index]);
-                    } else if (command[Math.ceil(index / 3) - 1].toLowerCase() == 'Order'.toLowerCase() && !isNaN(isProperNumber) && isProperNumber > 0) {
-                        orderQuantity.push(args[index]);
-                    } else if (command[Math.ceil(index / 3) - 1].toLowerCase() == 'budget'.toLowerCase()) {
-                        orderQuantity.push(args[index]);
-                    }
-                }
-            }
-        }
-
-        for (let index = 0; index < customerIngredientSign.length; index++) {
-            const result = takeOrder(command[index], customerIngredientSign[index], orderQuantity[index], customers, food, baseIngredients, restaurant);
+    if (initialString != undefined) {
+        const commandAndParameters: ICommandAndParameters[] = inputParserWithoutFile(initialString, baseIngredients);
+        restaurantBudgetIterations.push(restaurant.budget);
+        for (let index = 0; index < commandAndParameters.length; index++) {
+            const result = takeOrder(commandAndParameters[index], customers, food, baseIngredients, restaurant);
             restaurantBudgetIterations.push(restaurant.budget);
             finalOutput.push(result as string);
         }
     } else {
-        const input = inputParser('./src/txt_files/input.txt');
+        const input: ICommandAndParameters[] = inputParser('./src/txt_files/input.txt');
         for (let index = 0; index < input.length; index++) {
-            const result = takeOrder(input[index][0], input[index][1], input[index][2], customers, food, baseIngredients, restaurant);
+            const result = takeOrder(input[index], customers, food, baseIngredients, restaurant);
             restaurantBudgetIterations.push(restaurant.budget);
             finalOutput.push(result as string);
         }
@@ -89,6 +36,6 @@ export function main(...args: string[]) {
     createReport(restaurantBudgetIterations, finalOutput);
     return finalOutput;
 }
-// console.log(main());
-// console.log(main('buy', 'Adam Smith', 'Fries', 'buy', 'alexandra smith', 'Princess Chicken', 'order', 'tuna', '5'));
-console.log(main('budget', '=', '50', 'budget', '-', '50', 'budget', '+', '50'));
+console.log(main());
+// console.log(main(`buy, Adam Smith, Fries\nbuy, alexandra smith, Princess Chicken\norder, tuna, 5\nbudget, +, 15`));
+// console.log(main('budget', '=', '50', 'budget', '-', '50', 'budget', '+', '50'));
--- src/functions/takeOrder.ts
+++ src/functions/takeOrder.ts
@@ -1,32 +1,27 @@
 import { IBaseIngredients } from '../Interface/IBaseIngredients';
+import { ICommandAndParameters } from '../Interface/ICommandAndParameters';
 import { ICustomerAlergies } from '../Interface/ICustomerAlergies';
 import { IFood } from '../Interface/IFood';
 import { IRestaurant } from '../Interface/IRestaurant';
 import { possibleOutputs } from './possibleOutputs';
 
-export function takeOrder(
-    command: string,
-    customerIngredientSign: string,
-    orderQuantity: string,
-    customers: ICustomerAlergies[],
-    food: IFood[],
-    baseIngredients: IBaseIngredients[],
-    restaurant: IRestaurant
-) {
+export function takeOrder(commandAndParameters: ICommandAndParameters, customers: ICustomerAlergies[], food: IFood[], baseIngredients: IBaseIngredients[], restaurant: IRestaurant) {
     const matching: string[] = [];
     //Add variable to store information about total cost of order, set initial value to 0;
     let orderCost: number = 0;
     if (restaurant.budget >= 0) {
-        if (command.toLowerCase() == 'buy'.toLowerCase()) {
-            const specificCustomer = customers.find(customer => customer.customerName.toLowerCase() === customerIngredientSign.toLowerCase());
+        if (commandAndParameters.command.toLowerCase() == 'buy'.toLowerCase() && commandAndParameters.parameters != undefined) {
+            const specific = commandAndParameters.parameters[0];
+            const specificCustomer = customers.find(customer => customer.customerName.toLowerCase() === specific.toLowerCase());
             const alergies = specificCustomer?.alergies;
             if (specificCustomer == undefined) {
-                return `Sorry we can't handle your request ${customerIngredientSign}, we don't know about your alergies.`;
+                return `Sorry we can't handle your request ${specific}, we don't know about your alergies.`;
             } else {
-                if (!food.find(x => x.name.toLowerCase().includes(orderQuantity.toLowerCase()))) {
-                    return `Sorry we don't serve: ${orderQuantity}`;
+                const specificOrder = commandAndParameters.parameters[1];
+                if (!food.find(x => x.name.toLowerCase().includes(specificOrder.toLowerCase()))) {
+                    return `Sorry we don't serve: ${specificOrder}`;
                 } else {
-                    const orderedFood = food.find(x => x.name.toLowerCase().includes(orderQuantity.toLowerCase())) as IFood;
+                    const orderedFood = food.find(x => x.name.toLowerCase().includes(specificOrder.toLowerCase())) as IFood;
                     const orderedFoodIngredients = orderedFood?.ingerdients.map(ingredient => ingredient);
                     if (orderedFoodIngredients != undefined) {
                         while (orderedFoodIngredients.length > 0) {
@@ -50,33 +45,38 @@ export function takeOrder(
                     if (alergies != undefined) {
                         //Restaurant Markup
                         orderCost *= 1.3;
-                        const output = possibleOutputs(command, alergies, specificCustomer, orderedFood, orderCost, matching, restaurant);
+                        const output = possibleOutputs(commandAndParameters.command, alergies, specificCustomer, orderedFood, orderCost, matching, restaurant);
                         return output;
                     }
                 }
             }
         }
-        if (command.toLowerCase() == 'order'.toLowerCase()) {
+        if (commandAndParameters.command.toLowerCase() == 'order'.toLowerCase() && commandAndParameters.parameters != undefined) {
             //Find matching element price, store it, multiply it by amount of orders
+            const specificIngredient = commandAndParameters.parameters[0];
+            const amount = commandAndParameters.parameters[1];
             const singleUnit = baseIngredients.find(ingredient => {
-                if (ingredient.name.toLowerCase() == customerIngredientSign.toLowerCase()) {
-                    return ingredient.cost;
+                if (ingredient.name.trim().toLowerCase() == specificIngredient.trim().toLowerCase()) {
+                    return ingredient;
                 }
             });
+            console.log(specificIngredient);
             //Reduce restaurant budget by recived order
-            restaurant.budget -= singleUnit!.cost * parseFloat(orderQuantity);
-            return `We ordered ${parseFloat(orderQuantity)}x ${customerIngredientSign} and current restaurant budget is ${restaurant.budget.toFixed(2)}`;
+            restaurant.budget -= singleUnit!.cost * parseFloat(amount);
+            return `We ordered ${parseFloat(amount)}x ${specificIngredient} and current restaurant budget is ${restaurant.budget.toFixed(2)}`;
         }
-        if (command.toLowerCase() == 'budget'.toLowerCase()) {
-            if (customerIngredientSign == '=') {
-                restaurant.budget = parseFloat(orderQuantity);
+        if (commandAndParameters.command.toLowerCase() == 'budget'.toLowerCase() && commandAndParameters.parameters != undefined) {
+            const sign = commandAndParameters.parameters[0];
+            const amount = commandAndParameters.parameters[1];
+            if (sign == '=') {
+                restaurant.budget = parseFloat(amount);
                 return `New budget of restaurant: ${restaurant.budget}`;
-            } else if (customerIngredientSign == '-') {
-                restaurant.budget -= parseFloat(orderQuantity);
-                return `Budget of restaurant was reduced by: ${orderQuantity}, new budget is: ${restaurant.budget.toFixed(2)}`;
-            } else if (customerIngredientSign == '+') {
-                restaurant.budget += parseFloat(orderQuantity);
-                return `Budget of restaurant was increased by: ${orderQuantity}, new budget is: ${restaurant.budget.toFixed(2)}`;
+            } else if (sign == '-') {
+                restaurant.budget -= parseFloat(amount);
+                return `Budget of restaurant was reduced by: ${amount}, new budget is: ${restaurant.budget.toFixed(2)}`;
+            } else if (sign == '+') {
+                restaurant.budget += parseFloat(amount);
+                return `Budget of restaurant was increased by: ${amount}, new budget is: ${restaurant.budget.toFixed(2)}`;
             }
         }
     }
