4c0d439585494e758d33f4b3cdd4a93e4e6a6294
Cordione <52832668+Cordione@users.noreply.github.com>
2023-02-28 11:22:47 +0100

    rename to match purpose

---
+++ src/functions/outputs/buyOutput.ts
@@ -0,0 +1,24 @@
+import { ICustomerAlergies } from '../../Interface/ICustomerAlergies';
+import { IFood } from '../../Interface/IFood';
+import { IRestaurant } from '../../Interface/IRestaurant';
+
+export function buyOutput(alergies: string[], specificCustomer: ICustomerAlergies, orderedFood: IFood, orderCost: number, matching: string[], restaurant: IRestaurant) {
+    const matchingAlergies = alergies.some(x => matching.includes(x));
+    const listOfMatchingAlergies = alergies.filter(x => matching.includes(x));
+    if (matchingAlergies && specificCustomer.budget < orderCost) {
+        return `${specificCustomer.customerName} have budget: ${specificCustomer.budget} -> wants to order ${
+            orderedFood?.name
+        } -> can't order, food cost ${orderCost}, alergic to: ${listOfMatchingAlergies.join(' ').toLowerCase()}`;
+    } else if (matchingAlergies) {
+        return `${specificCustomer.customerName} have budget: ${specificCustomer.budget} -> wants to order ${orderedFood?.name} -> can't order, alergic to: ${listOfMatchingAlergies
+            .join(' ')
+            .toLowerCase()}`;
+    } else if (specificCustomer.budget < orderCost) {
+        return `${specificCustomer.customerName} have budget: ${specificCustomer.budget} -> wants to order ${orderedFood?.name} -> can’t order, ${orderedFood?.name} costs ${orderCost}`;
+    } else if (!matchingAlergies && specificCustomer.budget >= orderCost) {
+        const output = `${specificCustomer.customerName} have budget: ${specificCustomer.budget} -> wants to order ${orderedFood?.name}, which cost: ${orderCost.toFixed(2)}: success`;
+        specificCustomer.budget -= orderCost;
+        restaurant.budget += orderCost;
+        return output;
+    }
+}
