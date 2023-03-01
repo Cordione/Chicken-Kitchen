9ff74099e5e27251a15e96f7047e69c571d47f47
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-01 17:53:04 +0100

    Added test

--- src/test/ts/commandTokenizer.test.ts
+++ src/test/ts/commandTokenizer.test.ts
@@ -19,4 +19,21 @@ describe('Command Tokenizer tests', () => {
             { command: 'budget', parameters: ['+', '10'] },
         ]);
     });
+
+    test('Should properly handle output for table (single person/single order)', () => {
+        //given
+        const baseIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
+        const commandAndParameters: ICommandAndParameters[] = commandTokenizer(`table, Julie Mirage, Princess Chicken`, baseIngredients);
+        //when
+        //then
+        expect(commandAndParameters).toEqual([{ command: 'table', parameters: ['Julie Mirage', 'Princess Chicken'] }]);
+    });
+    test('Should properly handle output for table (2 persons/2 orders, affordable, w/o alergies)', () => {
+        //given
+        const baseIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
+        const commandAndParameters: ICommandAndParameters[] = commandTokenizer(`Table, Alexandra Smith, Adam Smith, Irish Fish, Fries`, baseIngredients);
+        //when
+        //then
+        expect(commandAndParameters).toEqual([{ command: 'Table', parameters: ['Alexandra Smith', 'Adam Smith', 'Irish Fish', 'Fries'] }]);
+    });
 });
