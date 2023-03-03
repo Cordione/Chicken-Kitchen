6ba9b25a98b0dfdae82f0b436b378e9836762358
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-03 04:41:18 +0100

    Added warehouse parser with needed interface

---
+++ src/functions/parsers/warehouseParser.ts
@@ -0,0 +1,36 @@
+import { IBaseIngredients } from '../../Interface/IBaseIngredients';
+import { IObjectInWarehouse } from '../../Interface/IObjectInWarehouse';
+import { doesFileExists } from '../utils/doesFileExists';
+import { baseIngredientsParser } from './baseIngredientsParser';
+import { createRawArray } from './utils/createRawArray';
+import { trimMe } from './utils/trimMe';
+
+export function warehouseParser(sourceString: string, baseIngredients: IBaseIngredients[]): IObjectInWarehouse[] {
+    //Warehouse parser should have sourceString with input.
+    //As we gonna have in either cases [{param1, param2} ...], we create interface for it, then we import it IObjectInWarehouse
+    const warehouseStockpile: IObjectInWarehouse[] = [];
+    //We also need array to store this n interfaces (content will be EACH of baseIngredients and food orders)
+    //We have to verify if this source string is proper
+    const doesFileExist = doesFileExists(sourceString);
+    //If yes -> take data and parse it
+    if (doesFileExist) {
+        const rawArray = createRawArray(sourceString);
+        for (let index = 0; index < rawArray.length; index++) {
+            const singleLine = rawArray[index].filter(x => x != '');
+            const secondToNColumn = trimMe(singleLine, 0);
+            if (singleLine.length > 1) {
+                warehouseStockpile.push({ name: singleLine[0], quantity: parseFloat(secondToNColumn[0]) });
+            }
+        }
+    }
+    //If no -> take default data
+    else {
+        console.log(baseIngredients);
+        baseIngredients.forEach(ingredient => {
+            warehouseStockpile.push({ name: ingredient.name, quantity: 5 });
+        });
+    }
+    return warehouseStockpile;
+}
+
+console.log(warehouseParser('./src/csv_files/warehouse.csv', baseIngredientsParser('./src/csv_files/baseIngredients.csv')));
