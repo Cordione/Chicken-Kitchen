d9ee7eebf21d5ee1feb8a89c19916a1b3e3bd3ae
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-07 14:12:40 +0100

    testing Audit command

--- src/test/ts/commandTokenizer.test.ts
+++ src/test/ts/commandTokenizer.test.ts
@@ -7,7 +7,7 @@ describe('Command Tokenizer tests', () => {
         //given
         const baseIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const commandAndParameters: ICommandAndParameters[] = commandTokenizer(
-            `Buy, Julie Mirage, Princess Chicken \n\r Buy, Elon Carousel, Tuna Cake \n\r Sadkl,jaslkdjasldkjaskldjsa\n\r buy, Julie Mirage,\n\r order, tuna, 5\n\r budget, +, 10`,
+            `Buy, Julie Mirage, Princess Chicken \n\r Buy, Elon Carousel, Tuna Cake \n\r Sadkl,jaslkdjasldkjaskldjsa\n\r buy, Julie Mirage,\n\r order, tuna, 5\n\r budget, +, 10\n\r Audit, Resources`,
             baseIngredients
         );
         //when
@@ -17,6 +17,7 @@ describe('Command Tokenizer tests', () => {
             { command: 'Buy', parameters: ['Elon Carousel', 'Tuna Cake'] },
             { command: 'order', parameters: ['tuna', '5'] },
             { command: 'budget', parameters: ['+', '10'] },
+            { command: 'Audit', parameters: ['Resources'] },
         ]);
     });
 
