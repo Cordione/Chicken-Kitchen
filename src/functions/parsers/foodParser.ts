188576260f9f2f20603ee7cb4fcca38db1e8845d
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-07 16:58:58 +0100

    added comments for foodParser

--- src/functions/parsers/foodParser.ts
+++ src/functions/parsers/foodParser.ts
@@ -4,9 +4,13 @@ import { baseIngredientsParser } from './baseIngredientsParser';
 import { createRawArray } from './utils/createRawArray';
 import { trimMe } from './utils/trimMe';
 export function foodParser(sourceString: string) {
+    
+    //Stage 1, Preparing Data
     const rawArray = createRawArray(sourceString);
     const foodArray: IFood[] = [];
     const baseIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
+    
+    //Stage 2, Split input into command strings
     for (let index = 0; index < rawArray.length; index++) {
         const singleLine = rawArray[index].filter(x => x != '');
         const secondToNColumn = trimMe(singleLine, 0);
@@ -14,6 +18,9 @@ export function foodParser(sourceString: string) {
             foodArray.push({ name: singleLine[0], ingerdients: secondToNColumn, price: 0, rawIngredients: [] });
         }
     }
+
+    //Stage 3: Bussiness logic
+
     countPriceOfOrder(foodArray, baseIngredients);
     return foodArray;
 }
