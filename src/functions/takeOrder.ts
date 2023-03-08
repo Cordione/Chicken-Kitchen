0e8c133e8e687aa0651a8614f99b625a353f876e
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-08 13:29:07 +0100

    Adjusted take Order to return optional parameter

--- src/functions/takeOrder.ts
+++ src/functions/takeOrder.ts
@@ -1,6 +1,6 @@
 import { IBaseIngredients } from '../Interface/IBaseIngredients';
 import { ICommandAndParameters } from '../Interface/ICommandAndParameters';
-import { ICommands } from '../Interface/ICommands';
+import { IInformationsFromJsonFile } from '../Interface/IInformationsFromJsonFIle';
 import { ICustomerAlergies } from '../Interface/ICustomerAlergies';
 import { IFood } from '../Interface/IFood';
 import { IObjectInWarehouse } from '../Interface/IObjectInWarehouse';
@@ -18,34 +18,34 @@ export function takeOrder(
     baseIngredients: IBaseIngredients[],
     restaurant: IRestaurant,
     warehouse: IObjectInWarehouse[],
-    commandOutput: ICommands
+    informationsFromJSONFile: IInformationsFromJsonFile
 ) {
     if (restaurant.budget >= 0) {
         //Add variable to store information about total cost of order, set initial value to 0;
-        const restaurantMarkup: number = 1.3;
+        const restaurantMarkup: number = informationsFromJSONFile.profitMargin != undefined ? parseFloat(`1.${informationsFromJSONFile.profitMargin}`) : 1.3;
         let output = '';
         if (commandAndParameters.command.toLowerCase() == 'buy'.toLowerCase() && commandAndParameters.parameters != undefined) {
-            if (commandOutput.buy == 'yes') {
-                return (output = buyOutput(commandAndParameters, customers, food, baseIngredients, restaurantMarkup, restaurant, warehouse) as string);
+            if (informationsFromJSONFile.buy == 'yes') {
+                return buyOutput(commandAndParameters, customers, food, baseIngredients, restaurantMarkup, restaurant, warehouse, informationsFromJSONFile);
             } else {
                 return `Buy command disabled.`;
             }
         } else if (commandAndParameters.command.toLowerCase() == 'order'.toLowerCase() && commandAndParameters.parameters != undefined) {
             //Find matching element price, store it, multiply it by amount of orders
-            if (commandOutput.order == 'yes') {
-                return (output = orderOutput(commandAndParameters, baseIngredients, restaurant, warehouse) as string);
+            if (informationsFromJSONFile.order == 'yes') {
+                return orderOutput(commandAndParameters, baseIngredients, restaurant, warehouse, informationsFromJSONFile);
             } else {
                 return `Order command disabled.`;
             }
         } else if (commandAndParameters.command.toLowerCase() == 'budget'.toLowerCase() && commandAndParameters.parameters != undefined) {
-            if (commandOutput.budget == 'yes') {
-                return (output = budgetOutput(commandAndParameters, restaurant));
+            if (informationsFromJSONFile.budget == 'yes') {
+                return budgetOutput(commandAndParameters, restaurant);
             } else {
                 return `Budget command disabled.`;
             }
         } else if (commandAndParameters.command.toLowerCase() == 'table'.toLowerCase()) {
-            if (commandOutput.table == 'yes') {
-                return (output = tableOutput(commandAndParameters, customers, food, baseIngredients, restaurantMarkup, restaurant, warehouse) as string);
+            if (informationsFromJSONFile.table == 'yes') {
+                return tableOutput(commandAndParameters, customers, food, baseIngredients, restaurantMarkup, restaurant, warehouse, informationsFromJSONFile);
             } else {
                 return `Table Command disabled.`;
             }
