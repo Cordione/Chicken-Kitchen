7dbbed133b3b77765fbacfd633cff0940f98052e
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-08 04:29:49 +0100

    Changed default json file to all commands accepted

--- src/functions/utils/commandJSONFileOutput.ts
+++ src/functions/utils/commandJSONFileOutput.ts
@@ -1,9 +1,8 @@
 import { ICommands } from '../../Interface/ICommands';
-import * as commands from '../../json/allEnabled.json';
 export function commandJSONFileOutput(jsonSource?: string) {
     let src;
     if (jsonSource == undefined || jsonSource == '') {
-        src = require(`../../json/commands.json`);
+        src = require(`../../json/allEnabled.json`);
     } else {
         src = require(jsonSource);
     }
