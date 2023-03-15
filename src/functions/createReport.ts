179e635189ced6f34a6e02c0fa1fc7047f615939
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-15 09:36:26 +0100

    report fix

--- src/functions/createReport.ts
+++ src/functions/createReport.ts
@@ -23,6 +23,10 @@ export function createReport(
 
     const maxBaseIngredientsQuantity = informationsFromJsonFile.maxIngredientType != undefined ? informationsFromJsonFile.maxIngredientType : 10;
     const maxBaseDishQuantity = informationsFromJsonFile.maxDishType != undefined ? informationsFromJsonFile.maxDishType : 3;
+    // if (index == 0 && !isRestaurantBankrupt) {
+    finalArray.push(`Restaurant budget: ${restaurantBudgetIterations[0]}`);
+    // }
+    restaurantBudgetIterations.splice(0, 1);
     for (let index = 0; index < outputArray.length; index++) {
         //If restaurant budget happends to be bellow 0 at any time, we declare it bankrupt, no future orders are realized/reported
         const wasted = whatWasWasted[index];
@@ -36,11 +40,13 @@ export function createReport(
         if (restaurantBudgetIterations[index] < 0) {
             isRestaurantBankrupt = true;
         }
-        if (index == 0 && !isRestaurantBankrupt) {
-            finalArray.push(`Restaurant budget: ${restaurantBudgetIterations[0].toFixed(2)}`);
+        if (restaurantBudgetIterations[index] >= 0) {
+            isRestaurantBankrupt = false;
         }
 
-        if (!isRestaurantBankrupt && whatWasWastedAsString[0] != undefined && whatWasWastedAsString[0].toLowerCase() != 'None'.toLowerCase()) {
+        finalArray.push(outputArray[index]);
+
+        if (whatWasWastedAsString[0] != undefined && whatWasWastedAsString[0].toLowerCase() != 'None'.toLowerCase()) {
             for (const element of wasted) {
                 const isBaseIngredient = baseIngredients.find(x => x.name.toLowerCase() === element.name.toLowerCase());
                 const isDish = food.find(x => x.name.toLowerCase() === element.name.toLowerCase());
@@ -52,15 +58,9 @@ export function createReport(
                 }
             }
         }
-        if (!isRestaurantBankrupt) {
-            finalArray.push(outputArray[index]);
-        }
-        if (index == outputArray.length - 1 && !isRestaurantBankrupt) {
-            finalArray.push(`Restaurant budget: ${restaurantBudgetIterations[restaurantBudgetIterations.length - 1].toFixed(2)}`);
-        }
 
-        if (isRestaurantBankrupt) {
-            finalArray.push(`RESTAURANT BANKRUPT`);
+        if (index == outputArray.length - 1 && !isRestaurantBankrupt) {
+            finalArray.push(`Restaurant budget: ${restaurantBudgetIterations[restaurantBudgetIterations.length - 1]}`);
         }
     }
     //Save array as file
