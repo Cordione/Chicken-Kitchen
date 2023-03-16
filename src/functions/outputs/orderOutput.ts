1a64976c952f59c273c4f88fe752a8d7a19480c5
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-16 17:47:20 +0100

    updated order outputs

--- src/functions/outputs/orderOutput.ts
+++ src/functions/outputs/orderOutput.ts
@@ -3,8 +3,9 @@ import { ICommandAndParameters } from '../../Interface/ICommandAndParameters';
 import { IFood } from '../../Interface/IFood';
 import { IInformationsFromJsonFile } from '../../Interface/IInformationsFromJsonFIle';
 import { IObjectInWarehouse } from '../../Interface/IObjectInWarehouse';
+import { IOrder } from '../../Interface/IOrder';
 import { IRestaurant } from '../../Interface/IRestaurant';
-import { handleOrder } from '../utils/handleOrder';
+import { handleOrder } from '../utils/taxHandlerForOrders';
 
 export function orderOutput(
     commandAndParameters: ICommandAndParameters,
@@ -15,6 +16,60 @@ export function orderOutput(
     informationsFromJSONFile: IInformationsFromJsonFile
 ) {
     const specificIngredient = commandAndParameters.parameters[0];
-    const amount = commandAndParameters.parameters[1];
-    return handleOrder(specificIngredient, amount, baseIngredients, food, restaurant, warehouse, informationsFromJSONFile);
+    const infromationsAboutOrders: IOrder[] = [];
+    const output: string[] = [];
+    let handledOrder;
+    //Check if commandsAndParameters.parameters length is divisible by 2 with 0 reminder
+    //If not return information that input is malformed
+    if (commandAndParameters.parameters.length % 2 != 0) {
+        return `Record is malformed, please pass equal amount of parameters for orders and quantities`;
+    } else if (commandAndParameters.parameters.length % 2 == 0) {
+        const halfOfLength = commandAndParameters.parameters.length / 2;
+        //create array of orders with quantities
+        for (let i = 0; i < commandAndParameters.parameters.length; i++) {
+            //Had to handle it this way
+            //i+2 was breaking a program.
+            if (i % 2 == 0) {
+                const orderName = commandAndParameters.parameters[i];
+                const orderQuantity = parseInt(commandAndParameters.parameters[i + 1]);
+                infromationsAboutOrders.push({ name: orderName, quantity: orderQuantity, tax: 0, totalCostWithTax: 0 });
+            }
+        }
+        //add tax info to previous array
+        for (let i = 0; i < infromationsAboutOrders.length; i++) {
+            handledOrder = handleOrder(infromationsAboutOrders[i].name, infromationsAboutOrders[i].quantity.toString(), baseIngredients, food, informationsFromJSONFile);
+            infromationsAboutOrders[i].tax = handledOrder[0] as number;
+            infromationsAboutOrders[i].totalCostWithTax = handledOrder[1] as number;
+        }
+        //Verify if we have inside only indexes allowed by JSON
+        for (let i = 0; i < infromationsAboutOrders.length; i++) {
+            const specificItem = infromationsAboutOrders[i].name;
+            const singleIngredient = baseIngredients.find(ingredient => ingredient.name.trim().toLowerCase() == specificItem.trim().toLowerCase());
+            const singleDish = food.find(dish => dish.name.trim().toLowerCase() == specificItem.trim().toLowerCase());
+            if (singleIngredient == undefined && singleDish == undefined) {
+                return `We recived malformed input, there's no such ingredient/dish as: ${specificItem}`;
+            }
+            if (singleIngredient == undefined && informationsFromJSONFile.order === 'ingredients') {
+                return `We recived malformed input, there's no such ingredient as: ${specificItem}`;
+            }
+            if (singleDish == undefined && informationsFromJSONFile.order === 'dishes') {
+                return `We recived malformed input, there's no such dish as: ${specificItem}`;
+            }
+        }
+        //update restaurant budget & warehouse stockpile
+        for (let i = 0; i < infromationsAboutOrders.length; i++) {
+            restaurant.budget -= infromationsAboutOrders[i].totalCostWithTax;
+            const stockpile = warehouse.find(x => x.name.toLowerCase() === infromationsAboutOrders[i].name.toLowerCase());
+            if (stockpile != undefined) {
+                stockpile.quantity = stockpile.quantity + infromationsAboutOrders[i].quantity;
+            } else {
+                warehouse.push({ name: infromationsAboutOrders[i].name, quantity: infromationsAboutOrders[i].quantity });
+            }
+            output.push(`We ordered ${infromationsAboutOrders[i].quantity}x ${infromationsAboutOrders[i].name} and current restaurant budget is ${restaurant.budget}`);
+        }
+    }
+    const taxMoney = infromationsAboutOrders.map(x => x.tax);
+    const totalTaxMoney = taxMoney.reduce((a, b) => a + b, 0);
+
+    return [output.join(', '), totalTaxMoney];
 }
--- src/test/ts/order.test.ts
+++ src/test/ts/order.test.ts
@@ -1,11 +1,11 @@
-import { orderOutput } from "../../functions/outputs/orderOutput";
-import { baseIngredientsParser } from "../../functions/parsers/baseIngredientsParser";
-import { customersParser } from "../../functions/parsers/customersParser";
-import { foodParser } from "../../functions/parsers/foodParser";
-import { warehouseParser } from "../../functions/parsers/warehouseParser";
-import { commandJSONFileOutput } from "../../functions/utils/commandJSONFileOutput";
-import { ICommandAndParameters } from "../../Interface/ICommandAndParameters";
-import { IRestaurant } from "../../Interface/IRestaurant";
+import { orderOutput } from '../../functions/outputs/orderOutput';
+import { baseIngredientsParser } from '../../functions/parsers/baseIngredientsParser';
+import { customersParser } from '../../functions/parsers/customersParser';
+import { foodParser } from '../../functions/parsers/foodParser';
+import { warehouseParser } from '../../functions/parsers/warehouseParser';
+import { commandJSONFileOutput } from '../../functions/utils/commandJSONFileOutput';
+import { ICommandAndParameters } from '../../Interface/ICommandAndParameters';
+import { IRestaurant } from '../../Interface/IRestaurant';
 
 describe('Ordering tests', () => {
     test('Order test, malformed input', () => {
@@ -22,9 +22,9 @@ describe('Ordering tests', () => {
         const json = commandJSONFileOutput(jsonSource);
         const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
-        
+
         //when
-        const result = orderOutput(input,  baseIngredients, food, restaurant, warehouse, json);
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
         //then
         expect(result).toContain("We recived malformed input, there's no such ingredient/dish as: Julie Mirage");
     });
@@ -42,11 +42,49 @@ describe('Ordering tests', () => {
         const json = commandJSONFileOutput(jsonSource);
         const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
-        
+
         //when
-        const result = orderOutput(input,  baseIngredients, food, restaurant, warehouse, json);
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
         //then
         expect(result).toContain("We recived malformed input, there's no such ingredient as: Fries");
+    });
+    test('Order test, 2 ingredients, 1 quantity - shouldnt work', () => {
+        const input: ICommandAndParameters = {
+            command: 'Order',
+            parameters: ['chicken', '1', 'tuna'],
+        };
+        const restaurant: IRestaurant = {
+            budget: 500,
+        };
+        const food = foodParser('./src/test/csv/food.csv');
+        const baseIngredients = baseIngredientsParser('./src/test/csv/baseIngredients.csv');
+        const jsonSource = '../../json/all.json';
+        const json = commandJSONFileOutput(jsonSource);
+        const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', baseIngredients, json);
+
+        //when
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
+        //then
+        expect(result).toContain('Record is malformed, please pass equal amount of parameters for orders and quantities');
+    });
+    test('Order test, 2 ingredients, 2 quantities - should work', () => {
+        const input: ICommandAndParameters = {
+            command: 'Order',
+            parameters: ['chicken', '1', 'princess chicken', '2'],
+        };
+        const restaurant: IRestaurant = {
+            budget: 500,
+        };
+        const food = foodParser('./src/test/csv/food.csv');
+        const baseIngredients = baseIngredientsParser('./src/test/csv/baseIngredients.csv');
+        const jsonSource = '../../json/all.json';
+        const json = commandJSONFileOutput(jsonSource);
+        const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', baseIngredients, json);
+
+        //when
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
+        //then
+        expect(result).toContain('We ordered 1x chicken and current restaurant budget is 478, We ordered 2x princess chicken and current restaurant budget is 280');
     });
     test('Order test, 1 ingredient - should work', () => {
         const input: ICommandAndParameters = {
@@ -62,12 +100,13 @@ describe('Ordering tests', () => {
         const json = commandJSONFileOutput(jsonSource);
         const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
-        
+
         //when
-        const result = orderOutput(input,  baseIngredients, food, restaurant, warehouse, json);
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
         //then
-        expect(result).toContain("We ordered 1x chicken and current restaurant budget is 478");
+        expect(result).toContain('We ordered 1x chicken and current restaurant budget is 478');
     });
+
     test('1 Dish with proper json', () => {
         const input: ICommandAndParameters = {
             command: 'Order',
@@ -82,12 +121,11 @@ describe('Ordering tests', () => {
         const json = commandJSONFileOutput(jsonSource);
         const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
-        console.log(json.order)
-        
+
         //when
-        const result = orderOutput(input,  baseIngredients, food, restaurant, warehouse, json);
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
         //then
-        expect(result).toContain("We ordered 1x Fries and current restaurant budget is 496");
+        expect(result).toContain('We ordered 1x Fries and current restaurant budget is 496');
     });
     test('1 Dish with proper json(all)', () => {
         const input: ICommandAndParameters = {
@@ -103,12 +141,11 @@ describe('Ordering tests', () => {
         const json = commandJSONFileOutput(jsonSource);
         const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
-        console.log(json.order)
-        
+
         //when
-        const result = orderOutput(input,  baseIngredients, food, restaurant, warehouse, json);
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
         //then
-        expect(result).toContain("We ordered 1x Fries and current restaurant budget is 496");
+        expect(result).toContain('We ordered 1x Fries and current restaurant budget is 496');
     });
     test('1 ingredient with proper json(all)', () => {
         const input: ICommandAndParameters = {
@@ -124,11 +161,31 @@ describe('Ordering tests', () => {
         const json = commandJSONFileOutput(jsonSource);
         const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
         const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
-        console.log(json.order)
-        
+
         //when
-        const result = orderOutput(input,  baseIngredients, food, restaurant, warehouse, json);
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
         //then
-        expect(result).toContain("We ordered 1x chicken and current restaurant budget is 478");
+        expect(result).toContain('We ordered 1x chicken and current restaurant budget is 478');
+    });
+    test('1 ingredient & 1 dish with wrong json(dish)', () => {
+        const input: ICommandAndParameters = {
+            command: 'Order',
+            parameters: ['chicken', '1', 'princess Chicken', '1'],
+        };
+        const restaurant: IRestaurant = {
+            budget: 500,
+        };
+        const food = foodParser('./src/test/csv/food.csv');
+        const baseIngredients = baseIngredientsParser('./src/test/csv/baseIngredients.csv');
+        const jsonSource = '../../json/dish.json';
+        const json = commandJSONFileOutput(jsonSource);
+        const allIngredients = baseIngredientsParser('./src/csv_files/baseIngredients.csv');
+        const warehouse = warehouseParser('./src/csv_files/warehouseSupplied.csv', allIngredients, json);
+        console.log(json.order);
+
+        //when
+        const result = orderOutput(input, baseIngredients, food, restaurant, warehouse, json);
+        //then
+        expect(result).toContain("We recived malformed input, there's no such dish as: chicken");
     });
 });
