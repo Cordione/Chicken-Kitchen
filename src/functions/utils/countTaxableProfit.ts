ef07b65810a000458b2e3ab1241e851568c90e47
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-27 15:19:21 +0200

    updated counting taxable profit to extempt them from taxation

--- src/functions/utils/countTaxableProfit.ts
+++ src/functions/utils/countTaxableProfit.ts
@@ -1,5 +1,3 @@
-import { IRestaurant } from "../../Interface/IRestaurant";
-
-export function countTaxableProfit(totalTaxPaid: number, budget: number[]){
-    return budget[budget.length -1] - budget[0] - totalTaxPaid
+export function countTaxableProfit(totalTaxPaid: number, budget: number[], tips: number) {
+    return budget[budget.length - 1] - budget[0] - totalTaxPaid - tips;
 }
