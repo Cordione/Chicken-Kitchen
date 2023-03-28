67c2a9ce7d419c802119200686b1b66feb45b410
Cordione <52832668+Cordione@users.noreply.github.com>
2023-03-28 16:26:30 +0200

    moved tax count one function

---
+++ src/functions/utils/dailyTax.ts
@@ -0,0 +1,14 @@
+import { IInformationsFromJsonFile } from '../../Interface/IInformationsFromJsonFIle';
+import { countTaxableProfit } from './countTaxableProfit';
+import { taxesToPay } from './taxesToPay';
+
+export function dailyTax(taxPaid: number[], tips: number[], budget: number[], json: IInformationsFromJsonFile) {
+    const tipsTaxFromJson = json.tipsTax != undefined ? json.tipsTax : 5;
+    const tipsTaxesAsDecimal = tipsTaxFromJson / 100;
+    const totalTaxPaid = taxPaid.reduce((a, b) => a + b, 0);
+    const totalTips = tips.flat().reduce((a, b) => a + b, 0);
+    const tipsTaxToPay = Math.ceil(totalTips * tipsTaxesAsDecimal);
+    const taxableProfit = countTaxableProfit(totalTaxPaid, budget, totalTips);
+    const dailyTaxAmount = taxesToPay(taxableProfit, json);
+    return [dailyTaxAmount, tipsTaxToPay];
+}
